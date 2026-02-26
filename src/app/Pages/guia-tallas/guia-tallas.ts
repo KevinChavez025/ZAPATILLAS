69ab16e2465c0e8e031dd8d77f6a96e683cf4067
KevinChavez025 <kevinchavezgeldres@gmail.com>
2026-02-26 01:39:18 -0500

    segun mi pepa que consumo

--- src/app/Pages/guia-tallas/guia-tallas.ts
+++ src/app/Pages/guia-tallas/guia-tallas.ts
@@ -90,7 +90,7 @@ export class GuiaTallas implements AfterViewInit {
       tallas: [22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38],
     },
     {
-      nombre: 'Urbano',
+      nombre: 'Escolar',
       rango:  'Tallas 27 – 38',
       edad:   '9 – 14 años',
       color:  '#089347',
