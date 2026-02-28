e7a4f7ad73fe0212a6e5173827b59791460d5658
Test User <test@example.com>
2026-02-27 22:32:34 -0500

    nosotros

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { Home }       from './Pages/home/home';
 import { Productos }  from './Pages/productos/productos';
 import { Tiendas }    from './Pages/tiendas/tiendas';
 import { GuiaTallas } from './Pages/guia-tallas/guia-tallas';
+import { Nosotros }   from './Pages/nosotros/nosotros';
 
 export const routes: Routes = [
   { path: '',            component: Home },
   { path: 'productos',   component: Productos },
   { path: 'tiendas',     component: Tiendas },
   { path: 'guia-tallas', component: GuiaTallas },
+  { path: 'nosotros',    component: Nosotros },
   { path: '**',          redirectTo: '' },
 ];
